cbe7a8a8d70bb3080eda23d9e815e58b71309558
mtaraing <88834940+mtaraing@users.noreply.github.com>
2021-08-12 19:34:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
     introTitle: 'Này đầu đất',
-    introDesc: `Em thấy nhớ anh, em
-    Em thấy rất nhớ anh thoi
+    introDesc: `Tớ thấy... cậu
+    Tớ thấy rất ... cậu thoi
     Ngày đêm cứ loay hoay
-    Trong suy nghĩ về anh
-    Anh có nhớ em không?
-    Anh có thấy nhớ em không?`,
+    Trong suy nghĩ về ...
+    Cậu có ... tớ không?
+    Cậu có thấy nhớ ... hog?`,
     btnIntro: 'Hic hic 🥺🥺',
-    title: 'Anh mà thoát ra coi chừng khỏi cho chơi game đấy 😌😌',
-    desc: 'Anh có yêu em nhiều hongggg 🥺🥺',
-    btnYes: 'Yêu nhiều lắm luôn, muốn ăn luôn 🥺🥺',
-    btnNo: 'Hong, anh yêu bé hàng xóm 😗😗',
-    question:'Vì sao anh lại yêu em dợoooo 🥺🥺',
-    btnReply: 'Gửi cho em nà 😝😝',
-    reply: 'Vì em là em bé của mình anh thoiiii 😘😘',
-    mess: 'Em biết mà, hihi 😚😚',
-    messDesc: 'Vậy thì bớt chơi game lại đi:))',
-    btnAccept: 'Dạ, anh biết gòi. Yêu emmmmmmm 🙆🙆',
+    title: 'Nè!!! Thoát ra coi chừng ăn dép đấy 😌😌',
+    desc: 'Cậu có thưn tớ nhìu hog? 🥺🥺',
+    btnYes: 'Thưn thưn nà 🥺🥺',
+    btnNo: 'Hong thưn á, được hog 😗😗',
+    question:'Vì sao cậu lại thưn tớ dợoooo 🥺🥺',
+    btnReply: 'Gửi cho tớ nà 😝😝',
+    reply: 'Vì cậu gất là cutee lại còn hiền nữa 😘😘',
+    mess: 'Tớ biết mà, hihi 😚😚',
+    messDesc: 'Vậy thì bớt mê tuii lại ik:))',
+    btnAccept: 'Hog nh 🙆🙆',
     messLink: 'http://fb.com' //messages/t/100014015913631
 }
